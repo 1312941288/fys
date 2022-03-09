834f383cc29c5538aa1eed3e29bbbdde1b1c929d
Enfys Wilson <yaojun.zhao@outlook.com>
2022-03-09 14:11:30 +0800

    first commit

---
+++ src/main/java/com/c/fys/FysApplication.java
@@ -0,0 +1,13 @@
+package com.c.fys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/c/fys/FysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c.fys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
